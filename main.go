958c21cac4adac55269e5d3c6f59a28685a6ca14
Joe Bologna <joebologna@mac.com>
2025-03-31 11:17:10 -0500

    render grid with row and cols

--- main.go
+++ main.go
@@ -18,10 +18,12 @@ func main() {
 	myWindow := myApp.NewWindow("Tip")
 	tileSize := float32(92)
 
-	size := fyne.NewSize(100, 30)
-	tiles := make([]fyne.CanvasObject, 0)
-	for row := float32(0); row < 5; row++ {
-		tiles = append(tiles, MakeTile(fmt.Sprintf("Group %d", int(row+1)), size, fyne.NewPos(0, (size.Height+2)*row)))
+	size := fyne.NewSize(80, 30)
+	entries := make([]fyne.CanvasObject, 0)
+	for col := float32(0); col < 4; col++ {
+		for row := float32(0); row < 5; row++ {
+			entries = append(entries, MakeEntry(fmt.Sprintf("Entry %d,%d", int(col+1), int(row+1)), size, fyne.NewPos((size.Width+2)*col, (size.Height+2)*row)))
+		}
 	}
 	pos := fyne.NewPos(0, (size.Height+2)*5)
 	button := widget.NewButton("update rows", func() {
@@ -31,8 +33,8 @@ func main() {
 	button.Importance = widget.HighImportance
 	button.Resize(size)
 	button.Move(pos)
-	tiles = append(tiles, button)
-	grid := container.NewWithoutLayout(tiles...)
+	entries = append(entries, button)
+	grid := container.NewWithoutLayout(entries...)
 	myWindow.SetContent(grid)
 
 	screenWidth := (tileSize + 5) * 4
@@ -42,7 +44,7 @@ func main() {
 	myWindow.ShowAndRun()
 }
 
-func MakeTile(text string, size fyne.Size, loc fyne.Position) fyne.CanvasObject {
+func MakeEntry(text string, size fyne.Size, loc fyne.Position) fyne.CanvasObject {
 	entry := widget.NewEntry()
 	entry.SetText(text)
 	entry.Resize(size)
