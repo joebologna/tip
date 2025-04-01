f759df88d3a5635e2447903b9e18ac0c9e98cdeb
Joe Bologna <joebologna@mac.com>
2025-04-01 08:43:05 -0500

    update entries in grid2 using button

--- appversion2.go
+++ appversion2.go
@@ -5,11 +5,14 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
 // Stub to generate a grid with entries and a button which updates them, resolving issues with AdaptiveGrid
 func App2() (*fyne.Container, *widget.Button) {
+	strings := make([]binding.String, 0)
+
 	// rows:=2
 	cols := 4
 
@@ -22,11 +25,17 @@ func App2() (*fyne.Container, *widget.Button) {
 	row++
 	grid2 := container.NewGridWithColumns(4)
 	for col := 0; col < cols; col++ {
-		grid2.Add(EntryCell(row, col))
+		entryString := binding.NewString()
+		strings = append(strings, entryString)
+		grid2.Add(EntryCell(row, col, entryString))
 	}
 
 	stuff := container.NewVBox(grid1, grid2)
-	button := widget.NewButton("stub button", func() {})
+	button := widget.NewButton("update grid2", func() {
+		for _, s := range strings {
+			s.Set("this")
+		}
+	})
 	return stuff, button
 }
 
@@ -34,8 +43,9 @@ func LabelCell(row, col int) fyne.CanvasObject {
 	return widget.NewLabel(fmt.Sprintf("Cell: %d,%d", row+1, col+1))
 }
 
-func EntryCell(row, col int) fyne.CanvasObject {
-	e := widget.NewEntry()
-	e.SetText(fmt.Sprintf("Cell: %d,%d", row+1, col+1))
+func EntryCell(row, col int, text binding.String) fyne.CanvasObject {
+	e := widget.NewEntryWithData(text)
+	e.Validator = nil
+	text.Set(fmt.Sprintf("Cell: %d,%d", row+1, col+1))
 	return e
 }
